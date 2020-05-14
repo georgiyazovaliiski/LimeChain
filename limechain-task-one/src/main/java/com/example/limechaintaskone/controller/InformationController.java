37639fbc17caab1bc0544bdddf5f5ebb002083f7
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2020-05-14 11:46:07 +0300

    Main Information Add

--- limechain-task-one/src/main/java/com/example/limechaintaskone/controller/InformationController.java
+++ limechain-task-one/src/main/java/com/example/limechaintaskone/controller/InformationController.java
@@ -12,10 +12,12 @@ public class InformationController {
     @RequestMapping
     public ResponseEntity getInformation(){
         return new ResponseEntity(
-        "You can play with the following options<br>" +
+        "<p>You can play with the following options<br>" +
                 "/burgers<br>" +
                 "with optional parameters for name, page(starting from zero), size.<br>" +
                 "Also you can try to get a random burger with its ingredients at /burgers/random.<br>" +
+                "There is a burger addition function which accepts name, imageUrl and ingredients.<br>" +
+                "The ingredients parameter should be a list of items, separated by a comma and(or) spaces</p>" +
                 "<h1>Have fun!!</h1>", HttpStatus.OK);
     }
 }
